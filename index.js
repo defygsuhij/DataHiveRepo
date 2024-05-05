b1657b53034d82ae0fc4e5e3731cbbf539ab78b1
defygsuhij <hsjcectored.rll@hotmail.com>
2024-05-05 17:21:15 +0700

    test project

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const message = "Hello world!";
-const messageLength = message.length;
-console.log(messageLength); // 13
+const senderSignature = aptos.transaction.sign({ signer: alice, transaction });
+
+// Sponsor signs
+const sponsorSignature = aptos.transaction.signAsFeePayer({
+  signer: sponsor,
+  transaction,
+});
+
+// Submit the transaction to chain
+const committedTxn = await aptos.transaction.submit.simple({
+  transaction,
+  senderAuthenticator: senderSignature,
+  feePayerAuthenticator: sponsorSignature,
+});
